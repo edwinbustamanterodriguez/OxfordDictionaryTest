5295894e455049d020bf8fab198ff4e0c7a20de5
Edwin Bustamante <edwin@xiobit.com>
2020-09-15 18:36:11 -0400

    crud implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :definitions
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
+
+
